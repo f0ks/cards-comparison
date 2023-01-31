ca41da3b27c80e4a43d98aac5d81c26d4461ab9a
Albert_Khamidullin <albert.khamidullin@firstlinesoftware.com>
2023-01-31 23:22:59 +0100

    cards working

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -3,7 +3,7 @@ import './card.scss';
 import {CardModel} from './card';
 import {CardSchema} from '../../interfaces/card-schema';
 
-function Card({data, schema}: {data: CardModel, schema: CardSchema[]}) {
+function Card({data, schema}: { data: CardModel, schema: CardSchema[] }) {
   useEffect(() => {
     // Your code here
   }, []);
@@ -12,14 +12,13 @@ function Card({data, schema}: {data: CardModel, schema: CardSchema[]}) {
     <div className="card">
       <h2>{data.title}</h2>
       <table>
-        <tr>
-          <th></th>
-          <th></th>
-        </tr>
-        <tr>
-          <td></td>
-        </tr>
+
       </table>
+      {schema.map((field, i) => 
+        <tr key={i}>
+          <td>{field.displayName}</td>
+          <td>{data.fields.find((item) => item.name === field.name)?.value} {field.units}</td>
+        </tr>)}
     </div>
   );
 }
--- src/interfaces/card-schema.d.ts
+++ src/interfaces/card-schema.d.ts
@@ -3,8 +3,8 @@
 export interface CardSchema {
   name: string,
   displayName: string,
-  //fieldType: 'numeric1' | 'enum', // figure out why it's not working
-  fieldType: 'numeric' | 'enum' | string,
+  // fieldType: 'numeric' | 'enum', // figure out why it's not working
+  fieldType: string,
   units?: string,
   options?: string[]
 }
